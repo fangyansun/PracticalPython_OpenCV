99731fbc035b8b3910ecbc218fcd7f44486444dc
Fangyan SUN <zhzxsunfangyan@yeah.net>
2018-10-12 12:51:40 +0200

    Signed-off-by: Fangyan SUN <zhzxsunfangyan@yeah.net>

--- simple_threasholding.py
+++ simple_threasholding.py
@@ -1,6 +1,5 @@
 import numpy as np
 import argparse
-import imutils
 import cv2
 
 ap = argparse.ArgumentParser()
@@ -8,9 +7,3 @@ ap.add_argument("-i", "--image", required = True, help = "Path to the image")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
-cv2.imshow("Original", image)
-cv2.waitKey(0)
-
-cropped = image[30:120, 80:150]
-cv2.imshow("Cropped", cropped)
-cv2.waitKey(0)
